062bb123b33da5a3a2c2f0249e707eab23bd793f
Vishal P <99859675+vishalpandhare01@users.noreply.github.com>
2026-04-29 04:36:01 +0000

    optimize way

--- app/view/blog_view.py
+++ app/view/blog_view.py
@@ -1,15 +1,28 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
-
 from app.model.user_model import User
 from app.schemas.blog_schemas import BlogCreate
 from app.model.blog_model import Blog 
+from sqlalchemy.orm import joinedload , load_only
 
 def get_blog(db: Session, blog_id: str):
     return db.query(Blog).filter(Blog.id == blog_id).first()
 
 def get_blogs_from_db(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Blog).offset(skip).limit(limit).all()
+    return(
+        db.query(Blog).
+        options(
+            joinedload(Blog.author).
+            load_only(
+                User.id,
+                User.name,
+                User.email
+            )
+        ).
+        offset(skip).
+        limit(limit).
+        all()
+    )
 
 def create_blog_in_db(db: Session, blog: BlogCreate, user_id: str):
     db_blog = Blog(title=blog.title, description=blog.description, text=blog.text, author_id=user_id)
@@ -19,7 +32,7 @@ def create_blog_in_db(db: Session, blog: BlogCreate, user_id: str):
     return db_blog
 
 def update_blog_in_db(db: Session, blog_id: str, user_id: str, blog: BlogCreate):
-    db_blog = db.query(Blog).options(joinedload(Blog.author).load_only("id", "name")).filter(Blog.id == blog_id, Blog.author_id == user_id).first()
+    db_blog = db.query(Blog).filter(Blog.id == blog_id, Blog.author_id == user_id).first()
     if db_blog:
         db_blog.title = blog.title
         db_blog.description = blog.description
