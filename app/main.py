bc85c21aae7255b3f2618baf133a32081529ca47
Vishal P <99859675+vishalpandhare01@users.noreply.github.com>
2026-05-01 04:24:30 +0000

    cors addes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import Depends, FastAPI, HTTPException
 from .routes.user_route import user_router
 from .routes.blog_route import blog_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # allowed domains
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],            # allow all headers
+)
 
 @app.get("/")
 def get_test():
